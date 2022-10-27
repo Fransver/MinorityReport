3c7e478019e69bd9b4499cfb8aaae09e802f1932
Verberne,Frans F.P.M <f.verberne@student.fontys.nl>
2022-10-27 11:07:14 +0200

    Cnn scraper data meegeven

--- scrapers/cnn_scraper.py
+++ scrapers/cnn_scraper.py
@@ -21,16 +21,24 @@ class CnnScraper:
     def scrape(self, dates_cnn, descr=True):
 
         for date in dates_cnn:
-            # Datum zit al inn de url van de artikelen, dus die format kon ik hier weglaten.
             url = "https://edition.cnn.com/articles/"
             soup = self.soup(self.req.get(url).content, 'html.parser')
             titles = soup.find_all('h3', class_='cd__headline')
-            # urls = soup.find_all('h3', class_='cd__headline') # Kan weg omdat url in zelfde class zit.
-            # heb die aangeroepen door in de for alleen het a component aan te roepen.
+
+            # # Datum zit al inn de url van de artikelen, dus die format kon ik hier weglaten.
+            # url = "https://edition.cnn.com/{}/{}/{}/".format(date.strftime("%Y"), date.strftime('%m'),
+            #                                                 date.strftime('%d'))
+            # soup = self.soup(self.req.get(url).content, 'html.parser')
+            # titles = soup.find_all('h1', class_='headline__text inline-placeholder')
+            # print(url)
+
+            # ========= Nu juiste jaar format, maar kan niet op url komen zonder specifieke url titel
+
+            # ============ DIT HOEFT NIET (na 1 uur achtergekomen :):), Zit al in datumstamp
 
             for i in range(len(titles)):
                 title = titles[i].text
-                url = "".join(["https://edition.cnn.com/articles", titles[i].a['href']]) # a class geselecteerd
+                url = "".join(["https://edition.cnn.com/", titles[i].a['href']])  # a class geselecteerd
 
                 if descr:
                     description = self.get_description(url)
@@ -56,10 +64,10 @@ class CnnScraper:
 scraper_cnn = CnnScraper()
 
 if __name__ == '__main__':
-
     articles = scraper_cnn.scrape(dates, descr=False)
 
     for article in articles:
         list_of_attribute_values = [article.title, article.description, article.date, article.url]
         list_of_lists.append(list_of_attribute_values)
         print(f"title: {article.title}\ndate: {article.date}\ndescription: {article.description}\nurl: {article.url}\n")
+
